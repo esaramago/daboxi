c2e1f1a5971987c765ad3bbf314a74ba9dc520f1
Emanuel Saramago <dev@emanuelsaramago.com>
2026-09-02 10:10:20 +0100

    feat: enhance Icon component to support additional sizes and improve className handling

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -8,7 +8,8 @@ const WaIcon = dynamic(() => import('@awesome.me/webawesome/dist/react/icon/inde
 interface Props {
   name?: string
   variant?: string
-  size?: 'small'
+  size?: 'small' | 's' | 'l' | 'xl' | '2xl' | '3xl'
+  className?: string
 }
 
 export default function Button(props: Props) {
@@ -16,12 +17,17 @@ export default function Button(props: Props) {
   const color = getColorByVariant(props.variant as Variant)
   const bgColor = color.replace('-50', '-80')
 
+  if (props.size === 'small') {
+    props.size = 's'
+  }
+  const fontSize = props.size ? `var(--wa-font-size-${props.size})` : ''
+
   return (
     <div
-      className={`c-icon ${props.size ? `c-icon--${props.size}` : ''}`}
+      className={`c-icon ${props.className || ''}`}
       style={{backgroundColor: bgColor || 'var(--wa-color-neutral-10)', color: color || 'var(--wa-color-neutral-50)'}}
     >
-      <WaIcon name={props.name || 'question'}></WaIcon>
+      <WaIcon name={props.name || 'question'} style={{fontSize: fontSize || null}}></WaIcon>
     </div>
   )
 }
