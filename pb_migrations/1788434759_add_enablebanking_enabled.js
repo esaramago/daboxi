a6f10e71fcdfa8bb2525a9157b60a0cf801a0adc
Emanuel Saramago <dev@emanuelsaramago.com>
2026-09-03 13:53:32 +0100

    feat: add migrationsDir option to pocketbase command and refactor enablebanking_enabled field addition

--- pb_migrations/1788434759_add_enablebanking_enabled.js
+++ pb_migrations/1788434759_add_enablebanking_enabled.js
@@ -2,19 +2,21 @@
 migrate((app) => {
   const collection = app.findCollectionByNameOrId("_pb_users_auth_")
 
-  // add field
-  collection.fields.addAt(12, new Field({
-    "autogeneratePattern": "",
-    "hidden": false,
-    "id": "bool2859811873",
-    "name": "enablebanking_enabled",
-    "presentable": false,
-    "required": false,
-    "system": false,
-    "type": "bool"
-  }))
+  const existing = collection.fields.getByName("enablebanking_enabled")
+  if (!existing) {
+    collection.fields.add(new Field({
+      "autogeneratePattern": "",
+      "hidden": false,
+      "id": "bool2859811873",
+      "name": "enablebanking_enabled",
+      "presentable": false,
+      "required": false,
+      "system": false,
+      "type": "bool"
+    }))
 
-  return app.save(collection)
+    return app.save(collection)
+  }
 }, (app) => {
   const collection = app.findCollectionByNameOrId("_pb_users_auth_")
 
